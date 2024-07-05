1b1a8b08031356b6ea90f49f267674340cc2e33e
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-07-05 11:40:04 +0200

    added variables

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let height  = Number(document.getElementById('height').value);
+let weight = Number(document.getElementById('weight').value);
+let bodyMass = weight / height/height*(10000);
+
+let results = document.getElementById('display');
+
+
+
+    
