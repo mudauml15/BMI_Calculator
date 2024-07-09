3306ef3f5c429b9a22bb0cc5d09268b9fb8458b3
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-07-09 13:29:19 +0200

    entered if statement for validating

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ let bodyMass = weight / height/height*(10000);
 let results = document.getElementById('display');
 
 
-if (bodyMass < 18.5) {
+if (bodyMass < 18.5  &&  height != isNaN  && weight != isNaN ) {
     document.getElementById('display').style.color= "orange";
     results.innerHTML = `Your body mass is ${bodyMass.toFixed(1)} kg (underweight)`
-    
-
-    
+  
 }
+ 
+  
 
-else if (bodyMass >= 18.5  && bodyMass <=  24.9  ){
+else if (bodyMass >= 18.5  && bodyMass <=  24.9 && height != isNaN  && weight != isNaN ){
     document.getElementById('display').style.color= "purple";
 
     results.innerHTML = `Your body mass is    ${bodyMass.toFixed(1)} kg  (Normal)`
+} 
 
 
-}
+    else if(bodyMass >= 18.5  && bodyMass <=  24.9 && height != isNaN  && weight != isNaN ){
+    document.getElementById('display').style.color= "purple";
 
-else  if (bodyMass >= 25  && bodyMass <= 29.9 )   {
+    results.innerHTML = `Your body mass is    ${bodyMass.toFixed(1)} kg  (Normal)`
+} 
+
+
+else  if (bodyMass >= 25  && bodyMass <= 29.9 && height != isNaN  && weight != isNaN )   {
     document.getElementById('display').style.color= "blue";
 
 
@@ -31,13 +37,25 @@ else  if (bodyMass >= 25  && bodyMass <= 29.9 )   {
 }
 
 
-else 
+
+
+
+else if( bodyMass >= 30 && height != isNaN  && weight != isNaN )  
 { 
     document.getElementById('display').style.color= "red";
 
     results.innerHTML = `Your body mass is  ${ bodyMass.toFixed(1)} kg ( obess)`
 }
+
+else {
+
+    document.getElementById('display').style.color= "black";
+
+    results.innerHTML = `enter valid inputs `
 }
+};
+
+
 
 function resetCalculation (){
         
@@ -45,7 +63,8 @@ function resetCalculation (){
     document.getElementById('weight').value = ' ';
     document.getElementById('display').innerHTML =' ';
     };
-   
+
+    
 
 
 
